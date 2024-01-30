f4a56cc396d0346df8c0bb33e154afc3905dbd5f
GyanGhoda <gyan.ghoda@outlook.com>
2024-01-30 16:08:57 -0500

    logger completed

--- quiz/scripts/logger.js
+++ quiz/scripts/logger.js
@@ -1,1 +1,16 @@
-// Define a JavaScript function called logMsg() that can be used to log an error message for any object that contains the property errMsg. 
+function logMsg(object) {
+    if (this.errMsg) {
+        console.log(`Error Message: " + ${object.errMsg}`);
+    }
+}
+
+const obj1 = {
+    errMsg: `Error in Obj1`
+}
+
+const obj2 = {
+    errMsg: `Error in Obj2`
+}
+
+logger.logMsg.call(obj1);
+logger.logMsg.call(obj2);
